3b0e41edb364a893e037b43f59a51d8a9492785a
ambroslins <ambros.lins@gmail.com>
2020-12-29 16:49:26 +0100

    Day 13 part 2

--- src/AdventOfCode/Day13.hs
+++ src/AdventOfCode/Day13.hs
@@ -1,7 +1,7 @@
 module AdventOfCode.Day13 where
 
 import AdventOfCode.Prelude
-import Data.List (maximumBy, minimumBy)
+import Data.List (minimumBy)
 import Data.Maybe (mapMaybe)
 
 data BusTabel = BusTabel Integer [Maybe Integer]
@@ -14,7 +14,7 @@ parseBusTabel =
     <*> (Just <$> decimal <|> Nothing <$ char 'x') `sepBy` char ','
 
 solution :: Solution
-solution = Solution parseBusTabel solve1 (const ("failed" :: String))
+solution = Solution parseBusTabel solve1 solve2
 
 solve1 :: BusTabel -> Integer
 solve1 (BusTabel x xs) =
@@ -24,8 +24,18 @@ solve1 (BusTabel x xs) =
 
 -- not a real solution, takes way too long.
 solve2 :: BusTabel -> Integer
-solve2 (BusTabel _ ms) = head $ foldr (\(y, o) ys -> filter (f y o) ys) (iterate (+ bus) (bus - offset)) xs
+solve2 (BusTabel _ ms) = crt equations
   where
-    (bus, offset) = maximumBy (compare `on` fst) xs
-    xs = mapMaybe (\(a, b) -> (,b) <$> a) $ zip ms [0 ..]
-    f x y z = (x - (z `mod` x)) `mod` x == y
+    equations = mapMaybe (\(a, b) -> (- a,) <$> b) $ zip [0 ..] ms
+
+crt :: [(Integer, Integer)] -> Integer
+crt xs =
+  (`mod` m) $
+    sum $
+      map (\(a, n) -> let b = m `div` n; (_, b', _) = egcd b n in a * b * b') xs
+  where
+    m = product $ map snd xs
+
+egcd :: Integer -> Integer -> (Integer, Integer, Integer)
+egcd 0 b = (b, 0, 1)
+egcd a b = let (g, x, y) = egcd (b `mod` a) a in (g, y - (b `div` a) * x, x)
