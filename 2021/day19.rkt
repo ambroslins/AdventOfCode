c9e33ade4efa41275e1284d0034444674f214a40
ambroslins <ambros.lins@gmail.com>
2021-12-19 22:33:44 +0100

    day 19: part 2

--- 2021/day19.rkt
+++ 2021/day19.rkt
@@ -1,7 +1,8 @@
 #lang typed/racket
 
 (require
-  math/matrix)
+  math/matrix
+  math/array)
 
 (define-type Scanner (Setof (Matrix Integer)))
 
@@ -56,17 +57,36 @@
               #:break trans)
     (if (>= (set-count (overlap t s1 s2)) 12) t #f)))
 
-(define (solve1 [scanners : (Listof Scanner)]) : Integer
-  (define (iter [fixed : Scanner] [ss : (Listof Scanner)]) : Scanner
-    (println (length ss))
+
+(define (solve [scanners : (Listof Scanner)]) : (Pairof (Listof transform) Scanner)
+  (define (iter [ts : (Listof transform)] [fixed : Scanner] [ss : (Listof Scanner)]) : (Pairof (Listof transform) Scanner)
     (match ss
       [(cons x xs)
        (let ([t (find-transform fixed x)])
-         (println t)
          (if t
-             (iter (set-union fixed (transform-scanner t x)) xs)
-             (iter fixed (append xs (list x)))))]
-      [(list) fixed]))
-  (set-count (iter (first scanners) (rest scanners))))
+             (iter (cons t ts) (set-union fixed (transform-scanner t x)) xs)
+             (iter ts fixed (append xs (list x)))))]
+      [(list) (cons ts fixed)]))
+  (iter '() (first scanners) (rest scanners)))
 
-(println (solve1 input))
+(define result : (Pairof (Listof transform) Scanner)
+  (solve input))
+
+(define solve1
+  (set-count (cdr result)))
+
+(println (set-count (cdr result)))
+
+(define (solve2 [transforms : (Listof transform)])
+  (for*/fold ([m : Integer 0])
+             ([t1 (in-list transforms)]
+              [t2 (in-list transforms)]) : Integer
+    (max m
+         (array-all-sum
+          (array-abs
+           (matrix-
+            (transform-translation t1)
+            (transform-translation t2)))))))
+
+(println (solve2 (car result)))
+
