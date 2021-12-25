94bda570689adb7c3731ae01f9e368594d1e8f60
ambroslins <ambros.lins@gmail.com>
2021-12-25 12:48:09 +0100

    day 24
    I was really frustrated with this problem at first,
    however I really  like this solution now.
    It is not super efficient but it is really general.
    Though some simplification steps are quite specialized

--- 2021/day24.rkt
+++ 2021/day24.rkt
@@ -1,5 +1,11 @@
 #lang racket
 
+; solution using hoare logic
+; basically we build a symbolic expression going through the instructions backwards
+; then we substitue the inital values and brute force the digits
+; we simplify the expressions to keep them as small as possible
+; and return early if no valid solution exists
+
 (require
   racket/file
   megaparsack megaparsack/text
@@ -43,24 +49,141 @@
   (for/list ([line (file->lines "inputs/24.txt")])
     (parse-result! (parse-string instruction/p line))))
 
-(define (exec state proc a b)
-  (let ([y (if (number? b) b (hash-ref state b))])
-    (hash-update state a (lambda (x) (proc x y)))))
+(define (recurse proc condition)
+  (define (rec a)
+    (recurse proc a))
+  (match condition
+    [(add a b) (proc (add (rec a) (rec b)))]
+    [(mul a b) (proc (mul (rec a) (rec b)))]
+    [(div a b) (proc (div (rec a) (rec b)))]
+    [(mod a b) (proc (mod (rec a) (rec b)))]
+    [(eql a b) (proc (eql (rec a) (rec b)))]
+    [else (proc condition)]))
 
-(define (solve digits state instructions)
-  (if (empty? instructions)
-      (if (= 0 (hash-ref state #\z)) digits #f)
-      (match (first instructions)
-        [(add a b) (solve digits (exec state + a b) (rest instructions))]
-        [(mul a b) (solve digits (exec state * a b) (rest instructions))]
-        [(div a b) (solve digits (exec state quotient a b) (rest instructions))]
-        [(mod a b) (solve digits (exec state remainder a b) (rest instructions))]
-        [(eql a b) (solve digits (exec state (lambda (x y) (if (= x y) 1 0)) a b) (rest instructions))]
-        [(inp a) (for/or ([d (in-inclusive-range 9 1 -1)])
-                   (solve (cons d digits) (hash-set state a d) (rest instructions))) ])))
 
-(define initial-state
-  (for/hash ([v (in-string "wxyz")]) (values v 0)))
+(define (subs condition x y)
+  (recurse
+   (lambda (c) (if (equal? c x) y c))
+   condition))
 
-(println (solve '() initial-state input))
+(struct interval (min max) #:transparent)
+
+(define (expr-interval expr)
+  (define (proc x)
+    (match x
+      [(add a b)
+       (and a b
+            (interval (+ (interval-min a) (interval-min b))
+                      (+ (interval-max a) (interval-max b))))]
+      [(mul a b)
+       (and a b
+            (interval (* (interval-min a) (interval-min b))
+                      (* (interval-max a) (interval-max b))))]
+      [(div a b)
+       (and a b
+            (interval (quotient (interval-min a) (interval-max b))
+                      (quotient (interval-max a) (interval-min b))))]
+      [(mod a b)
+       (or (and a b
+                (interval (if (< (interval-max a) (interval-max b)) (max 0 (interval-min a)) 0)
+                          (- (interval-max b) 1)))
+           (and b (interval 0 (- (interval-max b) 1))))]
+      [(eql a b) (interval 0 1)]
+      [(digit n) (interval 1 9)]
+      [(? number? a) (interval a a)]
+      [else #f]))
+  (recurse proc expr))
+
+; this is important to keep the conditions as small as possible
+(define (simplify condition)
+  (define (proc x)
+    (match x
+      [(eql (? number? a) (? number? b)) (if (= a b) 1 0)]
+      [(eql (? number? a) b) (simplify (eql b a))]
+      [(eql (add a (? number? b)) c) (simplify (eql a (add c (- b))))]
+      [(eql (mul a (? number? b)) (? number? c))
+       (let ([x (/ c b)])
+         (if (exact-integer? x)
+             (simplify (eql a x))
+             0)) ]
+      [(eql a b)
+       (=> continue)
+       (let ([in-a (expr-interval a)]
+             [in-b (expr-interval b)])
+         (if (and in-a in-b)
+             (cond
+               [(< (interval-max in-a) (interval-min in-b)) 0]
+               [(< (interval-max in-b) (interval-min in-a)) 0]
+               [else (continue)])
+             (continue)))]
+
+
+      [(add (? number? a) (? number? b)) (+ a b)]
+      [(add a 0) a]
+      [(add 0 b) b]
+
+      [(mul (? number? a) (? number? b)) (* a b)]
+      [(mul a 0) 0]
+      [(mul 0 b) 0]
+      [(mul a 1) a]
+      [(mul 1 b) b]
+
+      [(div (? number? a) (? number? b)) (quotient a b)]
+      [(div a 1) a]
+      [(div a b)
+       (=> continue)
+       (let ([in-a (expr-interval a)] [in-b (expr-interval b)])
+         (if (and in-a in-b
+                  (< (interval-max in-a) (interval-min in-b))
+                  (>= (interval-min in-a) 0))
+             0
+             (continue)))]
+
+
+      [(mod (? number? a) (? number? b)) (remainder a b)]
+      [(mod (mul a (? number? b)) (? number? c)) #:when (= b c) 0]
+
+      [(mod (add (mul a (? number? b)) c) (? number? d)) #:when (= b d) c]
+
+      [else x]))
+
+  (recurse proc condition))
+
+(struct digit (n) #:transparent)
+
+(define (make-step n)
+  (lambda (instruction post)
+    (simplify
+     (match instruction
+       [(add a b) (subs post a (add a b))]
+       [(mul a b) (subs post a (mul a b))]
+       [(div a b) (subs post a (div a b))]
+       [(mod a b) (subs post a (mod a b))]
+       [(eql a b) (subs post a (eql a b))]
+       [(inp a)
+        (set! n (+ n 1))
+        (subs post a (digit n))]))))
+
+(define (run instructions)
+  (define n -1)
+  (foldl (make-step n) (eql #\z 0) instructions))
+
+(define monad (simplify (subs (run (reverse input)) #\z 0)))
+
+; brute-force all the numbers
+; however this will stop early if no solution for a given prefix exists
+(define (model-number in-digit digits m)
+  (cond
+    [(equal? 1 m) digits]
+    [(equal? 0 m) #f]
+    [(> (length digits) 14) #f]
+    [else (for/or ([d in-digit])
+            (model-number
+             in-digit
+             (cons d digits)
+             (simplify (subs m (digit (- 13 (length digits))) d))))]))
+
+(println monad)
+(println (reverse (model-number (in-inclusive-range 9 1 -1) '() monad)))
+(println (reverse (model-number (in-inclusive-range 1 9) '() monad)))
 
