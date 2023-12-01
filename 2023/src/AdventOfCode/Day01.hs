3e2dc9b6c09e52f2856ca8169a571f439792029c
ambroslins <ambros.lins@gmail.com>
2023-12-01 07:58:10 +0100

    Solve day 1

--- 2023/src/AdventOfCode/Day01.hs
+++ 2023/src/AdventOfCode/Day01.hs
@@ -2,11 +2,40 @@ module AdventOfCode.Day01 (solution) where
 
 import AdventOfCode.Parser qualified as Parser
 import AdventOfCode.Prelude
+import Control.Monad (guard)
+import Data.ByteString.Char8 qualified as BS
+import Data.Char (isAlphaNum, isDigit)
+import Data.List (find)
+import Data.Maybe (mapMaybe)
 
 solution :: Solution
 solution =
   Solution
-    { parser = (sum <$> (Parser.decimal @Int `sepEndBy1` Parser.endOfLine)) `sepBy1` Parser.endOfLine,
-      part1 = maximum,
-      part2 = sum . take 3 . sortBy (flip compare)
+    { parser = Parser.takeWhile1 isAlphaNum `sepEndBy1` Parser.endOfLine,
+      part1 = solve parseDigit,
+      part2 = solve (\xs -> parseDigit xs <|> parseSpelledDigit xs)
     }
+
+solve :: (ByteString -> Maybe Int) -> [ByteString] -> Int
+solve f =
+  sum
+    . map
+      ( \xs ->
+          let digits = mapMaybe f (BS.tails xs)
+           in head digits * 10 + last digits
+      )
+
+parseDigit :: ByteString -> Maybe Int
+parseDigit line = do
+  (x, _) <- BS.uncons line
+  guard (isDigit x)
+  pure (read [x])
+
+parseSpelledDigit :: ByteString -> Maybe Int
+parseSpelledDigit line = do
+  (value, _) <-
+    find (\(_, spelled) -> spelled `BS.isPrefixOf` line) $
+      zip
+        [1 ..]
+        ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
+  pure value
