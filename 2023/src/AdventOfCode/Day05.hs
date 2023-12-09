e69493cecc1cd661c5aa2991e86cc75da1c680d3
ambroslins <ambros.lins@gmail.com>
2023-12-09 12:13:08 +0100

    Optimize day 5

--- 2023/src/AdventOfCode/Day05.hs
+++ 2023/src/AdventOfCode/Day05.hs
@@ -27,24 +27,23 @@ parseInput = do
   pure (seeds, maps)
   where
     pair = do
-      x <- Parser.decimal
-      Parser.whitespace
+      x <- Parser.decimal <* Parser.space
       y <- Parser.decimal
       pure (x, y)
 
 parseMap :: Parser Map
 parseMap = do
-  _name <- Parser.lexeme $ Parser.takeWhile1 (\c -> isAsciiLower c || c == '-')
-  Parser.symbol "map:" >> Parser.endOfLine
+  _name <- Parser.takeWhile1 (\c -> isAsciiLower c || c == '-') <* Parser.space
+  Parser.string "map:" >> Parser.endOfLine
   parseMapLine `sepEndBy'` Parser.endOfLine
 
 parseMapLine :: Parser (Range, Int)
 parseMapLine = do
-  destination <- Parser.lexeme Parser.decimal
-  source <- Parser.lexeme Parser.decimal
-  len <- Parser.lexeme Parser.decimal
-  let range = Interval.make source (source + len)
-      shift = destination - source
+  destination <- Parser.decimal <* Parser.space
+  source <- Parser.decimal <* Parser.space
+  len <- Parser.decimal
+  let !range = Interval.make source (source + len)
+      !shift = destination - source
   pure (range, shift)
 
 solve :: [Range] -> [Map] -> Int
