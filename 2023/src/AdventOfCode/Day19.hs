774a069476511fd8c6ebcd7be57ac85663254369
ambroslins <ambros.lins@gmail.com>
2023-12-20 21:40:03 +0100

    Solve day 19 part 2 (first try)

--- 2023/src/AdventOfCode/Day19.hs
+++ 2023/src/AdventOfCode/Day19.hs
@@ -1,18 +1,51 @@
 module AdventOfCode.Day19 (solution) where
 
+import AdventOfCode.Interval (Interval)
+import AdventOfCode.Interval qualified as Interval
 import AdventOfCode.Parser qualified as Parser
 import AdventOfCode.Prelude
+import Control.Monad.Identity (Identity (..))
 import Data.Char (isAsciiLower)
+import Data.Functor.Const (Const (..))
+import Data.HashMap.Strict ((!))
 import Data.HashMap.Strict qualified as HashMap
 
-data Part = Part {x, m, a, s :: !Int}
+type Lens s a = forall f. (Functor f) => (a -> f a) -> s -> f s
+
+get :: Lens s a -> s -> a
+get l = getConst . l Const
+
+set :: Lens s a -> a -> s -> s
+set l f = runIdentity . l (Identity . const f)
+
+data Part a = Part {x, m, a, s :: !a}
   deriving (Eq, Show)
+
+lens :: (s -> a) -> (s -> a -> s) -> Lens s a
+lens sa sas afa s = sas s <$> afa (sa s)
+
+x', m', a', s' :: Lens (Part a) a
+x' = lens x (\p x -> p {x})
+m' = lens m (\p m -> p {m})
+a' = lens a (\p a -> p {a})
+s' = lens s (\p s -> p {s})
 
 type Name = ByteString
 
-data Destination = Next !Name | Reject | Accept
+data Operation = LessThan | GreaterThan
+  deriving (Eq, Show)
 
-type Rule = Part -> Destination
+data Result = Next !Name | Reject | Accept
+  deriving (Eq, Show)
+
+data Rule = Rule
+  { field :: forall a. Lens (Part a) a,
+    operation :: !Operation,
+    value :: !Int,
+    result :: !Result
+  }
+
+type Workflow = ([Rule], Result)
 
 solution :: Solution
 solution =
@@ -23,41 +56,46 @@ solution =
         parts <- parsePart `sepEndBy'` Parser.endOfLine
         pure (HashMap.fromList ws, parts),
       part1 = uncurry solve1,
-      part2 = length . fst
+      part2 = uncurry solve2
     }
 
-parseWorkflow :: Parser (Name, Rule)
+parseWorkflow :: Parser (Name, Workflow)
 parseWorkflow = do
   name <- parseName
-  rule <- betweenCurly $ do
+  workflow <- betweenCurly $ do
     rules <- parseRule `sepEndBy'` Parser.char ','
-    dest <- parseDestination
-    pure $ foldr ($) (const dest) rules
-  pure (name, rule)
+    end <- parseResult
+    pure (rules, end)
+  pure (name, workflow)
 
-parseRule :: Parser (Rule -> Rule)
+parseRule :: Parser Rule
 parseRule = do
-  field <-
-    Parser.choice
-      [ x <$ Parser.char 'x',
-        m <$ Parser.char 'm',
-        a <$ Parser.char 'a',
-        s <$ Parser.char 's'
-      ]
-  op <- ((<) <$ Parser.char '<') <|> ((>) <$ Parser.char '>')
-  n <- Parser.decimal <* Parser.char ':'
-  dest <- parseDestination
-  pure $ \r part -> if field part `op` n then dest else r part
+  fieldName <- Parser.satisfy isAsciiLower
+  operation <- (LessThan <$ Parser.char '<') <|> (GreaterThan <$ Parser.char '>')
+  value <- Parser.decimal <* Parser.char ':'
+  result <- parseResult
+  pure $
+    Rule
+      { operation,
+        value,
+        result,
+        field = case fieldName of
+          'x' -> x'
+          'm' -> m'
+          'a' -> a'
+          's' -> s'
+          _ -> error $ "bad field name: " <> show fieldName
+      }
 
-parseDestination :: Parser Destination
-parseDestination =
+parseResult :: Parser Result
+parseResult =
   Parser.choice
     [ Accept <$ Parser.char 'A',
       Reject <$ Parser.char 'R',
       Next <$> parseName
     ]
 
-parsePart :: Parser Part
+parsePart :: Parser (Part Int)
 parsePart = betweenCurly $ do
   x <- "x=" *> Parser.decimal <* Parser.char ','
   m <- "m=" *> Parser.decimal <* Parser.char ','
@@ -71,15 +109,48 @@ parseName = Parser.takeWhile isAsciiLower
 betweenCurly :: Parser a -> Parser a
 betweenCurly = between (Parser.char '{') (Parser.char '}')
 
-solve1 :: HashMap Name Rule -> [Part] -> Int
-solve1 workflows = sum . map add . filter (apply workflows)
+solve1 :: HashMap Name Workflow -> [Part Int] -> Int
+solve1 workflows = sum . map add . filter (accepted (workflows ! "in"))
   where
     add Part {x, m, a, s} = x + m + a + s
-
-apply :: HashMap Name Rule -> Part -> Bool
-apply workflows = go (workflows HashMap.! "in")
-  where
-    go rule part = case rule part of
-      Next name -> go (workflows HashMap.! name) part
+    accepted :: Workflow -> Part Int -> Bool
+    accepted (rules, res) part = case rules of
+      [] -> apply part res
+      Rule {field, operation, value, result} : rs -> case operation of
+        LessThan
+          | get field part < value -> apply part result
+          | otherwise -> accepted (rs, res) part
+        GreaterThan
+          | get field part > value -> apply part result
+          | otherwise -> accepted (rs, res) part
+    apply :: Part Int -> Result -> Bool
+    apply part = \case
+      Next name -> accepted (workflows ! name) part
       Reject -> False
       Accept -> True
+
+solve2 :: HashMap Name Workflow -> [Part Int] -> Int
+solve2 workflows _ = uncurry (go start) (workflows ! "in")
+  where
+    start = let i = Interval.make 1 4001 in Part {x = i, m = i, a = i, s = i}
+    go :: Part (Interval Int) -> [Rule] -> Result -> Int
+    go !part rules !res = case rules of
+      [] -> apply part res
+      Rule {field, operation, value, result} : rs
+        | Interval.null interval -> 0
+        | otherwise -> case operation of
+            LessThan ->
+              let (left, right) = Interval.split value interval
+               in apply (set field left part) result
+                    + go (set field right part) rs res
+            GreaterThan ->
+              let (left, right) = Interval.split (value + 1) (get field part)
+               in apply (set field right part) result
+                    + go (set field left part) rs res
+        where
+          interval = get field part
+    apply :: Part (Interval Int) -> Result -> Int
+    apply part@Part {x, m, a, s} = \case
+      Next name -> uncurry (go part) (workflows ! name)
+      Reject -> 0
+      Accept -> Interval.size x * Interval.size m * Interval.size a * Interval.size s
--- 2023/src/AdventOfCode/Interval.hs
+++ 2023/src/AdventOfCode/Interval.hs
@@ -3,10 +3,12 @@ module AdventOfCode.Interval
     make,
     start,
     end,
+    null,
+    size,
     shift,
+    split,
     intersection,
     difference,
-    null,
   )
 where
 
@@ -33,6 +35,17 @@ difference a b = (left, right)
   where
     left = Interval {start = min (start a) (start b), end = min (end a) (start b)}
     right = Interval {start = max (start a) (end b), end = max (end a) (end b)}
+
+split :: (Ord a) => a -> Interval a -> (Interval a, Interval a)
+split x Interval {start, end} =
+  ( Interval {start, end = min x end},
+    Interval {start = max x start, end}
+  )
+
+size :: (Ord a, Num a) => Interval a -> a
+size Interval {start, end}
+  | start >= end = 0
+  | otherwise = end - start
 
 null :: (Ord a) => Interval a -> Bool
 null i = start i >= end i
