054200a471268e850e4879023af81003125de374
ambroslins <ambros.lins@gmail.com>
2024-01-01 12:35:14 +0100

    Day 25: tune parameters

--- 2023/src/AdventOfCode/Day25.hs
+++ 2023/src/AdventOfCode/Day25.hs
@@ -5,8 +5,9 @@ import AdventOfCode.Prelude
 import Control.Monad.State.Strict (runState)
 import Control.Monad.State.Strict qualified as State
 import Data.Char (isAsciiLower)
-import Data.HashMap.Lazy qualified as HashMap
+import Data.HashMap.Strict qualified as HashMap
 import Data.List qualified as List
+import Data.Map.Strict qualified as Map
 import Data.Ord (comparing)
 import Data.Vector.Storable qualified as Vector
 import Numeric.LinearAlgebra (Matrix)
@@ -15,7 +16,7 @@ import Numeric.LinearAlgebra qualified as LA
 solution :: Solution
 solution =
   Solution
-    { parser = parseLine `sepEndBy` Parser.endOfLine,
+    { parser = parseLine `sepEndBy'` Parser.endOfLine,
       solver = solve1 &&& const 0
     }
 
@@ -35,19 +36,27 @@ solve1 input = size * (n - size)
     n = LA.rows adj
     size = round $ Vector.sum cut
     pairs =
-      let m = 12
-       in LA.assoc (n, m) 0.0 $ do
-            i <- [0 .. m - 1]
-            [((0, i), 1.0), ((n - 1 - i, i), -1.0)]
-    weights = foldl' (\v a -> v + 0.5 * (a <> v)) pairs (replicate 15 adj)
+      let m = 10
+       in LA.assoc (n, m) 0.0 $
+            concatMap
+              (\i -> [((0, i), 1.0), ((n - 40 - i, i), -1.0)])
+              [0 .. m - 1]
+    weights = foldl' (flip (<>)) pairs $ replicate 10 adj
     cut =
-      head . List.maximumBy (comparing length) . List.group . List.sort $
-        LA.toColumns (LA.step weights)
+      fst
+        . List.maximumBy (comparing snd)
+        . Map.toList
+        . Map.fromListWith (+)
+        . map (,1 :: Int)
+        $ LA.toColumns (LA.step weights)
 
 adjacencyMatrix :: [(ByteString, [ByteString])] -> Matrix Double
 adjacencyMatrix xs = LA.assoc (nodes, nodes) 0.0 edges
   where
-    (edges, (_, nodes)) = runState (concat <$> traverse go xs) (HashMap.empty, 0)
+    (edges, (_, nodes)) =
+      runState
+        (concat <$> traverse go xs)
+        (HashMap.empty, 0)
     nodeIndex n = do
       (ns, i) <- State.get
       case HashMap.lookup n ns of
@@ -58,6 +67,4 @@ adjacencyMatrix xs = LA.assoc (nodes, nodes) 0.0 edges
     go (name, connections) = do
       i <- nodeIndex name
       js <- traverse nodeIndex connections
-      pure $ do
-        j <- js
-        [((i, j), 1.0), ((j, i), 1.0)]
+      pure $ concatMap (\j -> [((i, j), 1.0), ((j, i), 1.0)]) js
