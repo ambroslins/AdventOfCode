2e164ed599ae26fa6ae3a51dc711e8cbd0d5c1e9
ambroslins <ambros.lins@gmail.com>
2024-12-02 06:27:36 +0100

    Solve day 2

--- 2024/src/AdventOfCode/Day02.hs
+++ 2024/src/AdventOfCode/Day02.hs
@@ -2,11 +2,37 @@ module AdventOfCode.Day02 (solution) where
 
 import AdventOfCode.Parser qualified as Parser
 import AdventOfCode.Prelude
-import Data.ByteString qualified as BS
+import Data.List.NonEmpty (toList)
 
 solution :: Solution
 solution =
   Solution
-    { parser = Parser.line `sepEndBy` Parser.endOfLine,
-      solver = length &&& (maximum . map BS.length)
+    { parser = parseLine `sepEndBy` Parser.endOfLine,
+      solver = solve1 &&& solve2
     }
+
+parseLine :: Parser (NonEmpty Int)
+parseLine = Parser.int `sepEndBy1` Parser.whitespace
+
+solve1 = count safe . map toList
+  where
+    safe ls =
+      let diff = diffs ls
+       in all (\d -> d >= 1 && d <= 3) diff
+            || all (\d -> d <= -1 && d >= -3) diff
+
+solve2 = count (any safe . dropLevels) . map toList
+  where
+    safe ls =
+      let diff = diffs ls
+       in all (\d -> d >= 1 && d <= 3) diff
+            || all (\d -> d <= -1 && d >= -3) diff
+
+diffs (x : xs) = go x xs
+  where
+    go y (z : zs) = (z - y) : go z zs
+    go _ [] = []
+
+dropLevels :: [a] -> [[a]]
+dropLevels (x : xs) = xs : map (x :) (dropLevels xs)
+dropLevels [] = [[]]
