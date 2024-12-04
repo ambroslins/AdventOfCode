79743c593b06ddba5123ffba04ce8edcb1d0efa8
ambroslins <ambros.lins@gmail.com>
2024-12-04 17:19:15 +0100

    Solve day 4

--- 2024/src/AdventOfCode/Day04.hs
+++ 2024/src/AdventOfCode/Day04.hs
@@ -2,11 +2,36 @@ module AdventOfCode.Day04 (solution) where
 
 import AdventOfCode.Parser qualified as Parser
 import AdventOfCode.Prelude
-import Data.ByteString qualified as BS
+import Data.ByteString.Char8 qualified as BS
 
 solution :: Solution
 solution =
   Solution
-    { parser = Parser.line `sepEndBy` Parser.endOfLine,
-      solver = length &&& (maximum . map BS.length)
+    { parser = Parser.takeByteString,
+      solver = solve
     }
+
+solve :: ByteString -> (Int, Int)
+solve input =
+  ( sum $ map countPart1 indices,
+    count part2 indices
+  )
+  where
+    ncols =
+      fromMaybe (error "solve: expected '\\n' in input") $
+        BS.elemIndex '\n' input
+    indices = BS.elemIndices 'A' input
+    countPart1 i =
+      count
+        (== "XMAS")
+        [ [c | n <- [-2, -1, 0, 1], Just c <- [input BS.!? (i + n * (dx + dy))]]
+        | dx <- [-1, 0, 1],
+          dy <- [-ncols - 1, 0, ncols + 1]
+        ]
+    part2 i =
+      [ c
+      | dx <- [-1, 1],
+        dy <- [-ncols - 1, ncols + 1],
+        Just c <- [input BS.!? (i + dx + dy)]
+      ]
+        `elem` ["MMSS", "SSMM", "MSMS", "SMSM"]
