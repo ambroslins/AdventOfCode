7f7c59b95ad49cfe8f4427e9952b65fa8c3a23cd
ambroslins <ambros.lins@gmail.com>
2024-12-07 10:34:43 +0100

    Solve day 7

--- 2024/src/AdventOfCode/Day07.hs
+++ 2024/src/AdventOfCode/Day07.hs
@@ -2,11 +2,51 @@ module AdventOfCode.Day07 (solution) where
 
 import AdventOfCode.Parser qualified as Parser
 import AdventOfCode.Prelude
-import Data.ByteString qualified as BS
+import Data.List.NonEmpty qualified as NonEmpty
+
+data Equation = Equation {test :: !Int, numbers :: [Int]}
 
 solution :: Solution
 solution =
   Solution
-    { parser = Parser.line `sepEndBy` Parser.endOfLine,
-      solver = length &&& (maximum . map BS.length)
+    { parser = parseEquation `sepEndBy` Parser.endOfLine,
+      solver = solve
     }
+
+parseEquation :: Parser Equation
+parseEquation = do
+  test <- Parser.decimal
+  _ <- Parser.string ": "
+  numbers <- NonEmpty.toList <$> (Parser.decimal `sepBy1` Parser.char ' ')
+  pure $ Equation {test, numbers}
+
+solve :: [Equation] -> (Int, Int)
+solve equations =
+  ( sum $ map test $ filter couldBeTrue equations,
+    sum $ map test $ filter couldBeTrue2 equations
+  )
+
+couldBeTrue :: Equation -> Bool
+couldBeTrue Equation {test, numbers} = go 0 numbers
+  where
+    go !acc = \case
+      [] -> acc == test
+      n : ns -> go (acc + n) ns || go (acc * n) ns
+
+couldBeTrue2 :: Equation -> Bool
+couldBeTrue2 Equation {test, numbers} = go 0 numbers
+  where
+    go !acc = \case
+      [] -> acc == test
+      n : ns
+        | acc > test -> False
+        | otherwise -> go (acc + n) ns || go (acc * n) ns || go (acc ||| n) ns
+
+(|||) :: Int -> Int -> Int
+x ||| y = x * b + y
+  where
+    b
+      | y >= 1000 = 10000
+      | y >= 100 = 1000
+      | y >= 10 = 100
+      | otherwise = 10
