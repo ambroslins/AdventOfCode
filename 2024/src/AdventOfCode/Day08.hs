0d02b14391c5a0978f426520631f15ec41d2789f
ambroslins <ambros.lins@gmail.com>
2024-12-08 06:20:15 +0100

    Solve day 8

--- 2024/src/AdventOfCode/Day08.hs
+++ 2024/src/AdventOfCode/Day08.hs
@@ -1,12 +1,36 @@
 module AdventOfCode.Day08 (solution) where
 
-import AdventOfCode.Parser qualified as Parser
+import AdventOfCode.Grid (Grid)
+import AdventOfCode.Grid qualified as Grid
 import AdventOfCode.Prelude
-import Data.ByteString qualified as BS
+import Data.Map.Strict qualified as Map
+import Data.Set qualified as Set
+import Data.Vector.Unboxed (Vector)
+import Data.Vector.Unboxed qualified as Vector
 
 solution :: Solution
 solution =
   Solution
-    { parser = Parser.line `sepEndBy` Parser.endOfLine,
-      solver = length &&& (maximum . map BS.length)
+    { parser = Grid.parse,
+      solver = solve
     }
+
+solve :: Grid Vector Char -> (Int, Int)
+solve grid = (antinodes [1], antinodes [0 .. 100])
+  where
+    positions = Grid.findPositions (/= '.') grid
+    m =
+      Map.fromListWith (<>) $
+        [ (Grid.unsafeIndex grid p, [p])
+        | p <- Vector.toList positions
+        ]
+    antinodes range =
+      Set.size $ Set.fromList $ filter (`Grid.inside` grid) $ do
+        (_, ps) <- Map.toList m
+        Position row1 col1 <- ps
+        Position row2 col2 <- ps
+        let dr = row2 - row1
+            dc = col2 - col1
+        guard (dr /= 0 || dc /= 0)
+        n <- range
+        [Position (row1 - n * dr) (col1 - n * dc), Position (row2 + n * dr) (col2 + n * dc)]
