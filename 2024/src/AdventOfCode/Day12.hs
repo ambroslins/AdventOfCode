08ce1c81550f68f4bf970da8b19f2b07589d134c
Ambros Lins <ambros.lins@gmail.com>
2024-12-12 22:48:53 +0100

    Solve day 12 part 2

--- 2024/src/AdventOfCode/Day12.hs
+++ 2024/src/AdventOfCode/Day12.hs
@@ -4,7 +4,7 @@ import AdventOfCode.Grid (Grid)
 import AdventOfCode.Grid qualified as Grid
 import AdventOfCode.Position qualified as Pos
 import AdventOfCode.Prelude
-import Control.Monad (forM_)
+import Control.Monad (foldM, forM_, when)
 import Control.Monad.ST.Strict (runST)
 import Data.STRef.Strict (newSTRef, readSTRef, writeSTRef)
 import Data.Vector.Unboxed qualified as VU
@@ -22,6 +22,7 @@ solve labels = runST $ do
   regions <- Grid.newMutable @VU.Vector nrows ncols (-1 :: Int)
   areas <- MVU.replicate (nrows * ncols) (0 :: Int)
   perimeters <- MVU.replicate (nrows * ncols) (0 :: Int)
+  sides <- MVU.replicate (nrows * ncols) (0 :: Int)
   nextRegion <- newSTRef (0 :: Int)
 
   let mark !region !label !pos = do
@@ -31,11 +32,13 @@ solve labels = runST $ do
           else do
             Grid.write regions pos region
             MVU.modify areas (+ 1) region
-            forM_ [North, South, East, West] $ \dir ->
-              let !p = Pos.move dir pos
-               in if Grid.index labels p == Just label
-                    then mark region label p
-                    else MVU.modify perimeters (+ 1) region
+            let move s dir =
+                  let !p = Pos.move dir pos
+                   in if Grid.index labels p == Just label
+                        then mark region label p $> s
+                        else pure (s + 1)
+            s <- foldM move 0 [North, South, East, West]
+            MVU.modify perimeters (+ s) region
       newRegion !label !pos = do
         r <- Grid.read regions pos
         if r >= 0
@@ -48,10 +51,42 @@ solve labels = runST $ do
   VU.forM_ (Grid.findPositions (const True) labels) $ \pos ->
     newRegion (Grid.unsafeIndex labels pos) pos
 
+  forM_ [0 .. nrows] $ \row -> forM_ [0 .. ncols] $ \col -> do
+    let reg pos = fromMaybe (-1) <$> Grid.readMaybe regions pos
+    a <- reg $ Position {row = row - 1, col = col - 1}
+    b <- reg $ Position {row = row - 1, col = col}
+    c <- reg $ Position {row = row, col = col - 1}
+    d <- reg $ Position {row = row, col = col}
+
+    when (a >= 0 && (a /= b && a /= c || a == b && a == c && a /= d)) $
+      MVU.modify sides (+ 1) a
+    when (b >= 0 && (b /= d && b /= a || b == a && b == d && b /= c)) $
+      MVU.modify sides (+ 1) b
+    when (c >= 0 && (c /= d && c /= a || c == a && c == d && c /= b)) $
+      MVU.modify sides (+ 1) c
+    when (d >= 0 && (d /= b && d /= c || d == b && d == c && d /= a)) $
+      MVU.modify sides (+ 1) d
+
   as <- VU.unsafeFreeze areas
   ps <- VU.unsafeFreeze perimeters
-  let price = VU.sum $ VU.zipWith (*) as ps
+  ss <- VU.unsafeFreeze sides
+  let !price1 = VU.sum $ VU.zipWith (*) as ps
+      !price2 = VU.sum $ VU.zipWith (*) as ss
 
-  pure (price, 0)
+  pure (price1, price2)
   where
     (nrows, ncols) = Grid.size labels
+
+{-
+1 : 4  => 4
+3 : 8  => 6
+4 : 8 => 4
+5 : 12  => 6
+14 : 28 => 22
+10 : 18 => 12
+11 : 20 => 12
+12 : 18 => 10
+13 : 20 => 10
+13 : 18 => 8
+14 : 22 => 16
+ -}
