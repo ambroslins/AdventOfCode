25e8596b28324a613a7bec7612ea01f91667980e
Ambros Lins <ambros.lins@gmail.com>
2024-12-19 07:58:42 +0100

    Solve day 19 part 1

--- 2024/src/AdventOfCode/Day19.hs
+++ 2024/src/AdventOfCode/Day19.hs
@@ -2,11 +2,43 @@ module AdventOfCode.Day19 (solution) where
 
 import AdventOfCode.Parser qualified as Parser
 import AdventOfCode.Prelude
+import Control.Monad (foldM)
+import Control.Parallel.Strategies (parMap, rseq)
 import Data.ByteString qualified as BS
+import Data.Char (isAsciiLower)
+import Data.HashSet qualified as HashSet
+
+type Towel = ByteString
+
+type Design = ByteString
 
 solution :: Solution
 solution =
   Solution
-    { parser = Parser.line `sepEndBy` Parser.endOfLine,
-      solver = length &&& (maximum . map BS.length)
+    { parser = do
+        towels <- parseTowel `sepBy'` Parser.symbol ", "
+        Parser.endOfLine
+        Parser.endOfLine
+        designs <- parseDesing `sepEndBy'` Parser.endOfLine
+        pure (towels, designs),
+      solver = uncurry solve1 &&& length . snd
     }
+
+solve1 :: [Towel] -> [Design] -> Int
+solve1 towels = count id . parMap rseq (possible towels)
+
+parseTowel :: Parser Towel
+parseTowel = Parser.takeWhile1 isAsciiLower
+
+parseDesing :: Parser Design
+parseDesing = Parser.takeWhile1 isAsciiLower
+
+possible :: [Towel] -> Design -> Bool
+possible towels = isNothing . go HashSet.empty
+  where
+    go !seen design
+      | BS.null design = Nothing
+      | design `HashSet.member` seen = Just seen
+      | otherwise =
+          foldM go (HashSet.insert design seen) $
+            mapMaybe (`BS.stripPrefix` design) towels
