ad08615ae415e2edf61957fab3275420e3e4c84e
Ambros Lins <ambros.lins@gmail.com>
2025-12-01 17:40:34 +0100

    solve day 01

--- 2025/src/AdventOfCode/Day01.hs
+++ 2025/src/AdventOfCode/Day01.hs
@@ -2,7 +2,6 @@ module AdventOfCode.Day01 (solution) where
 
 import AdventOfCode.Parser qualified as Parser
 import AdventOfCode.Prelude
-import Data.ByteString qualified as BS
 
 solution :: Solution
 solution =
@@ -11,8 +10,19 @@ solution =
       solver = solve
     }
 
-parseLine :: Parser ByteString
-parseLine = Parser.line
+parseLine :: Parser Int
+parseLine = do
+  dir <- Parser.anyChar
+  distance <- Parser.decimal
+  pure $ if dir == 'L' then negate distance else distance
 
-solve :: [ByteString] -> (Int, Int)
-solve ls = (length ls, BS.length $ head ls)
+solve :: [Int] -> (Int, Int)
+solve rs = let (_, !p1, !p2) = foldl' go (50, 0, 0) rs in (p1, p2)
+  where
+    go (!dial, !p1, !p2) rot =
+      let (!d, !m) = (dial + rot) `divMod` 100
+          !isZero = if m == 0 then 1 else 0
+       in ( m,
+            p1 + isZero,
+            if rot >= 0 then p2 + d else p2 - d - fromEnum (dial == 0) + isZero
+          )
