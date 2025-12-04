cf89b9c67fc552356a587d128178d85f54892ff2
Ambros Lins <ambros.lins@gmail.com>
2025-12-04 22:56:14 +0100

    optimize day 04

--- 2025/src/AdventOfCode/Day04.hs
+++ 2025/src/AdventOfCode/Day04.hs
@@ -1,5 +1,3 @@
-{-# HLINT ignore "Use for_" #-}
-{-# LANGUAGE MultiWayIf #-}
 {-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
 
 {-# HLINT ignore "Parenthesize unary negation" #-}
@@ -9,12 +7,10 @@ module AdventOfCode.Day04 (solution) where
 import AdventOfCode.Parser qualified as Parser
 import AdventOfCode.Prelude
 import Control.Monad (unless, when)
-import Control.Monad.ST (runST)
 import Data.ByteString qualified as BS
 import Data.Char qualified as Char
 import Data.Int (Int8)
 import Data.Massiv.Array qualified as M
-import Data.Massiv.Array.Unsafe (unsafeFreeze)
 import Data.Massiv.Core (Ix2 (..))
 import Data.Monoid (Sum (..))
 import Data.Word (Word8)
@@ -41,44 +37,39 @@ c2w :: Char -> Word8
 c2w = fromIntegral . Char.ord
 
 solve :: M.Array M.U Ix2 Int8 -> (Int, Int)
-solve grid = (countRemoved ns, solve2 ns)
+solve grid = (solve1 neighbours, solve2 neighbours)
   where
-    ns = M.compute @M.U $ neighboursM grid
-
-countRemoved :: (M.Index ix, M.Source r Int8) => M.Array r ix Int8 -> Int
-countRemoved = getSum . M.foldMono (\i -> if i < 4 then Sum 1 else Sum 0)
-
-solve2 :: (M.Manifest r Int8) => M.Array r Ix2 Int8 -> Int
-solve2 grid = countRemoved $ M.withMArrayST_ grid $ \mg -> do
-  let go !ix = do
-        n <- M.readM mg ix
-        when (n >= 0 && n < 4) $ do
-          M.writeM mg ix (-1)
-          for (-1) 1 $ \dr ->
-            for (-1) 1 $ \dc ->
-              unless (dr == 0 && dc == 0) $ do
-                let !next = ix + (dr :. dc)
-                M.read mg next >>= \case
-                  Nothing -> pure ()
-                  Just m
-                    | m < 0 || m >= 100 -> pure ()
-                    | otherwise -> do
-                        M.modify_ mg (\x -> pure $ x - 1) next
-                        go next
-
-  M.iforM_ grid $ \ix i -> when (i < 4) $ go ix
-  pure ()
-
-neighboursM :: (M.Manifest r Int8) => M.Array r Ix2 Int8 -> M.Array M.DW Ix2 Int8
-neighboursM = M.mapStencil (M.Fill 0) stencil
-  where
+    neighbours = M.compute @M.U $ M.mapStencil (M.Fill 0) stencil grid
     stencil = M.makeStencil (M.Sz (3 :. 3)) (1 :. 1) $ \get ->
       get (-1 :. -1)
         + get (-1 :. 0)
         + get (-1 :. 1)
         + get (0 :. -1)
-        + (1 - get (0 :. 0)) * 100
+        + (1 - get (0 :. 0)) * 100 -- we use values >= 100 to indicate empty fields
         + get (0 :. 1)
         + get (1 :. -1)
         + get (1 :. 0)
         + get (1 :. 1)
+
+solve1 :: (M.Index ix, M.Source r Int8) => M.Array r ix Int8 -> Int
+solve1 = getSum . M.foldMono (\i -> if i < 4 then Sum 1 else Sum 0)
+
+solve2 :: (M.Manifest r Int8) => M.Array r Ix2 Int8 -> Int
+solve2 grid = solve1 $ M.withMArrayST_ grid $ \mg ->
+  let go !ix =
+        M.read mg ix >>= \case
+          Nothing -> pure ()
+          Just n
+            | n < 0 || n >= 4 -> pure ()
+            | otherwise -> do
+                M.writeM mg ix (-1)
+                for (-1) 1 $ \dr ->
+                  for (-1) 1 $ \dc ->
+                    unless (dr == 0 && dc == 0) $
+                      let !next = ix + (dr :. dc)
+                       in M.modify mg (\x -> pure $ x - 1) next >>= \case
+                            Nothing -> pure ()
+                            Just m
+                              | m < 0 || m > 4 -> pure ()
+                              | otherwise -> go next
+   in M.iforM_ grid $ \ix n -> when (n < 4) $ go ix
