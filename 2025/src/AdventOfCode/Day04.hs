790f2a20fba92d11e73c8309e5cf2aacaf721aa3
Ambros Lins <ambros.lins@gmail.com>
2025-12-04 19:35:49 +0100

    optimize day 04 using massiv

--- 2025/src/AdventOfCode/Day04.hs
+++ 2025/src/AdventOfCode/Day04.hs
@@ -2,68 +2,83 @@
 {-# LANGUAGE MultiWayIf #-}
 {-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
 
+{-# HLINT ignore "Parenthesize unary negation" #-}
+
 module AdventOfCode.Day04 (solution) where
 
-import AdventOfCode.Grid qualified as Grid
+import AdventOfCode.Parser qualified as Parser
 import AdventOfCode.Prelude
-import Control.Monad (forM_, unless, when)
+import Control.Monad (unless, when)
+import Control.Monad.ST (runST)
+import Data.ByteString qualified as BS
+import Data.Char qualified as Char
 import Data.Int (Int8)
-import Data.Vector.Unboxed (Vector)
-import Data.Vector.Unboxed qualified as Vector
+import Data.Massiv.Array qualified as M
+import Data.Massiv.Array.Unsafe (unsafeFreeze)
+import Data.Massiv.Core (Ix2 (..))
+import Data.Monoid (Sum (..))
+import Data.Word (Word8)
 
 solution :: Solution
 solution =
   Solution
-    { parser = Grid.parse,
+    { parser = parseArray,
       solver = solve
     }
 
-solve :: Grid.Grid Vector Char -> (Int, Int)
-solve grid = (solve1 ns, solve2 ns)
+parseArray :: (M.Load r Ix2 Int8) => Parser (M.Array r Ix2 Int8)
+parseArray = do
+  bs <- Parser.takeByteString
+  ncols <- maybe (fail "no end of line") pure $ BS.elemIndex (c2w '\n') bs
+  let (nrows, rest) = BS.length bs `divMod` (ncols + 1)
+      roll = c2w '@'
+  unless (rest == 0) $ fail "not rectangular"
+  pure $
+    M.makeArray M.Seq (M.Sz (nrows :. ncols)) $
+      \(row :. col) -> if BS.index bs (row * (ncols + 1) + col) == roll then 1 else 0
+
+c2w :: Char -> Word8
+c2w = fromIntegral . Char.ord
+
+solve :: M.Array M.U Ix2 Int8 -> (Int, Int)
+solve grid = (countRemoved ns, solve2 ns)
   where
-    ns = neighbours grid
+    ns = M.compute @M.U $ neighboursM grid
 
-solve1 :: Grid.Grid Vector Int8 -> Int
-solve1 = countVector (< 4) . Grid.cells
+countRemoved :: (M.Index ix, M.Source r Int8) => M.Array r ix Int8 -> Int
+countRemoved = getSum . M.foldMono (\i -> if i < 4 then Sum 1 else Sum 0)
 
-countVector :: (Vector.Unbox a) => (a -> Bool) -> Vector a -> Int
-countVector p = Vector.foldl' (\acc x -> if p x then acc + 1 else acc) 0
-
-solve2 :: Grid.Grid Vector Int8 -> Int
-solve2 grid = countVector (< 0) $ Grid.cells $ Grid.create $ do
-  g <- Grid.thaw grid
-  let go !pos = do
-        n <- Grid.read g pos
+solve2 :: (M.Manifest r Int8) => M.Array r Ix2 Int8 -> Int
+solve2 grid = countRemoved $ M.withMArrayST_ grid $ \mg -> do
+  let go !ix = do
+        n <- M.readM mg ix
         when (n >= 0 && n < 4) $ do
-          Grid.write g pos (-1)
+          M.writeM mg ix (-1)
           for (-1) 1 $ \dr ->
             for (-1) 1 $ \dc ->
               unless (dr == 0 && dc == 0) $ do
-                let !p = Position {row = row pos + dr, col = col pos + dc}
-                when (Grid.inside p grid) $ do
-                  Grid.modify g (\x -> x - 1) p
-                  go p
+                let !next = ix + (dr :. dc)
+                M.read mg next >>= \case
+                  Nothing -> pure ()
+                  Just m
+                    | m < 0 || m >= 100 -> pure ()
+                    | otherwise -> do
+                        M.modify_ mg (\x -> pure $ x - 1) next
+                        go next
 
-  Vector.mapM_ go $ Grid.findPositions (< 4) grid
-  pure g
+  M.iforM_ grid $ \ix i -> when (i < 4) $ go ix
+  pure ()
 
-neighbours :: Grid.Grid Vector Char -> Grid.Grid Vector Int8
-neighbours grid = Grid.create $ do
-  g <- Grid.newMutable nrows ncols 127
-  for 0 (ncols - 1) $ \c -> do
-    for 0 (nrows - 1) $ \r -> do
-      let p = Position {row = r, col = c}
-      when (Grid.unsafeIndex grid p == '@') $ do
-        let !n =
-              count (== '@') $
-                mapMaybe
-                  (Grid.index grid)
-                  [ Position {row = r + dr, col = c + dc}
-                  | dc <- [-1, 0, 1],
-                    dr <- [-1, 0, 1],
-                    dc /= 0 || dr /= 0
-                  ]
-        Grid.write g p (fromIntegral n)
-  pure g
+neighboursM :: (M.Manifest r Int8) => M.Array r Ix2 Int8 -> M.Array M.DW Ix2 Int8
+neighboursM = M.mapStencil (M.Fill 0) stencil
   where
-    (nrows, ncols) = Grid.size grid
+    stencil = M.makeStencil (M.Sz (3 :. 3)) (1 :. 1) $ \get ->
+      get (-1 :. -1)
+        + get (-1 :. 0)
+        + get (-1 :. 1)
+        + get (0 :. -1)
+        + (1 - get (0 :. 0)) * 100
+        + get (0 :. 1)
+        + get (1 :. -1)
+        + get (1 :. 0)
+        + get (1 :. 1)
