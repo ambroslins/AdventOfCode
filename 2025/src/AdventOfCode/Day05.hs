b8935401767f068a327afd3a37cc7d90bc8a4883
Ambros Lins <ambros.lins@gmail.com>
2025-12-06 01:01:15 +0100

    simplify day 05

--- 2025/src/AdventOfCode/Day05.hs
+++ 2025/src/AdventOfCode/Day05.hs
@@ -3,6 +3,7 @@ module AdventOfCode.Day05 (solution) where
 import AdventOfCode.Parser qualified as Parser
 import AdventOfCode.Prelude
 import Data.IntMap.Strict qualified as IntMap
+import Data.List qualified as List
 
 solution :: Solution
 solution =
@@ -23,26 +24,19 @@ parseRange = do
   pure (start, end)
 
 solve :: [(Int, Int)] -> [Int] -> (Int, Int)
-solve ranges ids = (count inRange ids, IntMap.foldlWithKey' go 0 rangeMap)
+solve ranges ids = (count inRange ids, IntMap.foldlWithKey' sumSize 0 rangeMap)
   where
     rangeMap = mergeRanges ranges
     inRange !i = case IntMap.lookupLE i rangeMap of
       Nothing -> False
       Just (_, end) -> i <= end
-    go !acc !start !end = acc + end - start + 1
+    sumSize !acc !start !end = acc + end - start + 1
 
 mergeRanges :: [(Int, Int)] -> IntMap Int
-mergeRanges = foldl' go IntMap.empty
+mergeRanges = merge IntMap.empty . List.sort
   where
-    go !m (!start, !end) =
-      let left = IntMap.lookupLE start m
-          right = IntMap.lookupGT start m
-          s = case left of
-            Just (sl, el) | start <= el -> sl -- overlap the left range
-            _ -> start
-          e = case right of
-            Just (sr, er) | end >= sr -> max end er -- overlap the right range
-            _ -> maybe end (max end . snd) left
-       in IntMap.insert s e $ case right of
-            Just (sr, _) | end >= sr -> IntMap.delete sr m
-            _ -> m
+    merge !m [] = m
+    merge !m [(start, end)] = IntMap.insert start end m
+    merge !m ((s1, e1) : (s2, e2) : rest)
+      | e1 >= s2 = merge m ((s1, max e1 e2) : rest)
+      | otherwise = merge (IntMap.insert s1 e1 m) $ (s2, e2) : rest
