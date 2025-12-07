52d1b4d038aef90b8ceb10ed2cf628a0d898a217
Ambros Lins <ambros.lins@gmail.com>
2025-12-07 19:00:25 +0100

    optimize day 07

--- 2025/src/AdventOfCode/Day07.hs
+++ 2025/src/AdventOfCode/Day07.hs
@@ -2,31 +2,39 @@ module AdventOfCode.Day07 (solution) where
 
 import AdventOfCode.Parser qualified as Parser
 import AdventOfCode.Prelude
+import Control.Monad (foldM)
+import Control.Monad.ST.Strict (runST)
 import Data.ByteString qualified as BS
-import Data.IntMap.Strict qualified as IntMap
-import Data.IntSet qualified as IntSet
+import Data.Vector.Primitive.Mutable qualified as MVector
 
 solution :: Solution
 solution =
   Solution
-    { parser = do
-        l : ls <- Parser.lines
-        let start = fromMaybe (error "no start") $ BS.elemIndex (c2w 'S') l
-        pure (start, filter (not . null) $ map (BS.elemIndices (c2w '^')) ls),
-      solver = uncurry solve
+    { parser = Parser.lines,
+      solver = solve
     }
 
-solve :: Int -> [[Int]] -> (Int, Int)
-solve start ls = second sum $ foldl' go (0, IntMap.singleton start 1) splitters
+solve :: [ByteString] -> (Int, Int)
+solve [] = error "no input"
+solve (l : ls) = runST $ do
+  timelines <- MVector.replicate width 0
+  MVector.write timelines start 1
+  let go !acc !i = do
+        t <- MVector.read timelines i
+        if t == 0
+          then pure acc
+          else do
+            MVector.write timelines i 0
+            MVector.modify timelines (+ t) (i - 1)
+            MVector.modify timelines (+ t) (i + 1)
+            pure $ acc + 1
+  hits <- foldM go 0 splitters
+  ts <- MVector.foldl' (+) 0 timelines
+  pure (hits, ts)
   where
-    splitters = map IntSet.fromDistinctAscList ls
-    go (!acc, !beams) splitter =
-      let hits = IntMap.restrictKeys beams splitter
-          splits =
-            IntMap.unionWith
-              (+)
-              (IntMap.mapKeysMonotonic (subtract 1) hits)
-              (IntMap.mapKeysMonotonic (+ 1) hits)
-       in ( acc + IntMap.size hits,
-            IntMap.unionWith (+) (IntMap.difference beams hits) splits
-          )
+    width = BS.length l
+    start = fromMaybe (error "no start") $ BS.elemIndex (c2w 'S') l
+    splitters = concatMap (BS.elemIndices (c2w '^')) $ everyOther ls
+    everyOther = \case
+      _ : x : xs -> x : everyOther xs
+      _ -> []
