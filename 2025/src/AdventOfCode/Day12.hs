cfdd942db5c99ae607b107196d869eacabd5ecf4
Ambros Lins <ambros.lins@gmail.com>
2025-12-13 13:35:57 +0100

    solve day 12 (estimated)

--- 2025/src/AdventOfCode/Day12.hs
+++ 2025/src/AdventOfCode/Day12.hs
@@ -7,12 +7,35 @@ import Data.ByteString qualified as BS
 solution :: Solution
 solution =
   Solution
-    { parser = parseLine `sepEndBy'` Parser.endOfLine,
-      solver = solve
+    { parser = do
+        shapes <- parseShape `sepEndBy'` Parser.endOfLine
+        regions <- parseRegion `sepEndBy'` Parser.endOfLine
+        pure (shapes, regions),
+      solver = uncurry solve
     }
 
-parseLine :: Parser ByteString
-parseLine = Parser.line
+data Region = Region {width :: !Int, height :: !Int, quantities :: [Int]}
+  deriving (Show)
 
-solve :: [ByteString] -> (Int, Int)
-solve ls = (length ls, BS.length $ head ls)
+parseShape :: Parser Int
+parseShape = do
+  _ <- Parser.digit
+  _ <- Parser.char ':'
+  Parser.endOfLine
+  bs <- Parser.take 12
+  pure $ BS.foldl' (\acc c -> acc + if c == c2w '#' then 1 else 0) 0 bs
+
+parseRegion :: Parser Region
+parseRegion = do
+  width <- Parser.decimal
+  _ <- Parser.char 'x'
+  height <- Parser.decimal
+  _ <- Parser.symbol ":"
+  quantities <- Parser.decimal `sepBy'` Parser.whitespace
+  pure Region {width, height, quantities}
+
+solve :: [Int] -> [Region] -> (Int, Int)
+solve _ regions = (count estimate regions, 0)
+  where
+    estimate Region {width, height, quantities} =
+      width * height >= foldl' (\acc q -> acc + 9 * q) 0 quantities
